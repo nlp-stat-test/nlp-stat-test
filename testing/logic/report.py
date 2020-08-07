00d113659908c25139ca5218ad1feef121e7e91c
DeniseMak <dpm3@uw.edu>
2020-08-07 14:18:10 -0700

    checkpoint

--- testing/logic/report.py
+++ testing/logic/report.py
@@ -8,6 +8,16 @@ def truefalseToYesNo(true_or_false):
   else:
     return 'Unknown'
 
+def getReasonsTable(dictSignificanceReasons):
+  table_string = ''
+  for test, reason in dictSignificanceReasons.items():
+    table_string = table_string + '* ' +   str(test) + ": {}\n".format(reason)
+  return table_string
+
+def getSigtestBootstrap():
+  sigtestBootstrap = '200'
+  return sigtestBootstrap
+
 def gen_report(options: dict, string: str):
   report_string = """
 # System Performance Report
@@ -17,16 +27,26 @@ def gen_report(options: dict, string: str):
 Tests indicate the following about the data in the file `""" + options["filename"] + """`:
   * Is the data distribution normal? """ + truefalseToYesNo(options["normality_message"])  + """
   * The skewness measurement is: """ + options["skewness_message"] + """
-  * Based on skewness and normality, the recommended statistic to use for each evaluation unit is: """ + options["test_statistic_message"] + """
+  * Based on skewness and normality, the recommended statistic to use for each evaluation unit is: """ + \
+                  options["test_statistic_message"] + """
 
 
 Based on this information, the following significance tests are appropriate for your data:
 
-""" + options["significance_tests_table"] + """
+""" + getReasonsTable(options["significance_tests_table"]) + """
 
 ## Significance testing
 
-Requiring a significance level $\\alpha = """ + options["significance_alpha"] + """$, """ + options["bootstrap iterations"] + """ iterations for bootstraping tests""" +  """ and using the """ + options["chosen_sig_test"] + """ significance test, can you reject the null hypothesis? """ + truefalseToYesNo(options["should_reject?"]) + """ The test statistic or confidence interval = """ + options["statistic/CI"] + """ for the significance test you selected.
+###Significance test parameters:
+* Significance level alpha = """ + options["significance_alpha"] +\
+                  '\n * Bootstrap iterations: ' + \
+                  options["bootstrap iterations"]  + '\n' + \
+                  """
+                  * Significance test name = """ + options["chosen_sig_test"] + '\n' + """
+                  * Can you reject the null hypothesis? """ + '\n' + \
+                  truefalseToYesNo(options["should_reject?"]) + """
+                  * Test statistic or confidence interval = """ + \
+                  options["statistic/CI"] + """
 
 ## Effect Size
 
