308ffc6cce773bea4ef02940ee501c3d7f159cf9
haz060 <37172891+haz060@users.noreply.github.com>
2020-08-16 16:38:44 -0700

    Update powerAnalysis.py

--- testing/logic/powerAnalysis.py
+++ testing/logic/powerAnalysis.py
@@ -15,7 +15,7 @@ from matplotlib import pyplot as plt
 plt.rcParams['svg.fonttype'] = 'none'
 
 
-def post_power_analysis(sig_test_name, method, score, num_of_subsample, dist_name, B, alpha, mu, output_dir, alternative, boot_B = None):
+def post_power_analysis(sig_test_name, method, score, num_of_subsample, dist_name, B, alpha, mu, output_dir, alternative="two-sided", boot_B = None):
 
 	def get_sim_sample_sizes(z, num_of_subsample):
 		partitions = np.array_split(range(len(z)), num_of_subsample)
@@ -44,7 +44,7 @@ def post_power_analysis(sig_test_name, method, score, num_of_subsample, dist_nam
 					for j in range(0,len(z_b)):
 						z_b_dict[j] = z_b[j]
 
-					(test_stats, pval, CI, rejection) = sigTesting.run_sig_test("t", z_b_dict, alpha, boot_B, mu, alternative)
+					(test_stats, pval, rejection) = sigTesting.run_sig_test("t", z_b_dict, alpha, boot_B, mu, alternative) # TO-FIX add CI
 					if rejection:
 						count+=1
 				power_sampsizes[i] = float(count)/B
@@ -60,7 +60,7 @@ def post_power_analysis(sig_test_name, method, score, num_of_subsample, dist_nam
 				z_b_dict = {}
 				for j in range(0,len(z_b)):
 					z_b_dict[j] = z_b[j]
-				(test_stats, pval, CI, rejection) = sigTesting.run_sig_test(sig_test_name, z_b_dict, alpha, boot_B, mu, alternative)
+				(test_stats, pval, rejection) = sigTesting.run_sig_test(sig_test_name, z_b_dict, alpha, boot_B, mu, alternative) # TO-FIX add CI
 				if rejection:
 					count+=1
 			power_sampsizes[i] = float(count)/B
