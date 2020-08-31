deb954d948ac205a5a2c3905b7576ca929b07cdd
haz060 <37172891+haz060@users.noreply.github.com>
2020-08-31 16:09:28 -0700

    print results to report file

--- testing/main.py
+++ testing/main.py
@@ -4,10 +4,12 @@ import logic.sigTesting
 import logic.testCase
 import logic.effectSize
 import logic.powerAnalysis
+import logic.power_analysis_norm
 
 import sys
 import time
 import numpy as np
+import os
 
 import yaml
 
@@ -17,12 +19,17 @@ if __name__ == '__main__':
 	score_file = sys.argv[1] # load score file
 
 	config_file = sys.argv[2] # load config file
+
+	####################################################
+	####################################################
+
+	## read in config files
+
 	with open(config_file, 'r') as ymlfile:
 		config = yaml.load(ymlfile)
 
 	with open('logic/sysconfig.yml', 'r') as ymlfile:
 		sysconfig = yaml.load(ymlfile)
-
 
 	### initialize a new testCase object
 	testCase_new = logic.testCase.testCase(None, None, None, None, None)
@@ -53,13 +60,51 @@ if __name__ == '__main__':
 
 	# output dir
 	fig_output_dir = str(config['fig_output_dir'])
-	#report_output_dir = str(config['report_output_dir'])
+	report_output_dir = str(config['report_output_dir'])
+
+	if not os.path.exists(report_output_dir):
+		os.makedirs(report_output_dir)
 
 	# null hypothesis
 	mu = float(config['mu'])
 
 	# h1
 	h1 = str(config['h1'])
+
+
+	####################################################
+	####################################################
+
+	## pre-testing power analysis
+
+	pre_testing_power = input("To conduct pre-testing power analysis for normal data?: yes/no \n")
+
+	if pre_testing_power == 'yes':
+		pre_mu_diff = float(config['pre_mu_diff'])
+		pre_sd = float(config['pre_sd'])
+		pre_pow_lev = float(config['pre_pow_lev'])
+		pre_alpha = float(config['pre_alpha'])
+		pre_alternative = str(config['pre_alternative'])
+
+		pre_samp_size = logic.power_analysis_norm.prosp_power_analysis_norm(pre_mu_diff, pre_sd, pre_pow_lev, pre_alpha, pre_alternative)
+
+
+		####################################
+		with open(report_output_dir +'/report','a') as report_file:
+			report_file.write('Pre-testing power analysis result:\n')
+			report_file.write('-- mean difference: '+str(pre_mu_diff)+'\n')
+			report_file.write('-- standard deviation: '+str(pre_sd)+'\n')
+			report_file.write('-- desired power level: '+str(pre_pow_lev)+'\n')
+			report_file.write('-- significance level: '+str(pre_alpha)+'\n')
+			report_file.write('-- alternative hypothesis: '+str(pre_alternative)+'\n')
+			report_file.write('-- minimally required sample size: '+str(pre_samp_size)+'\n\n')
+		####################################
+
+		print('------ Pre-testing power analysis finished ------')
+
+	####################################################
+	####################################################
+
 
 
 	### read score file
@@ -96,14 +141,44 @@ if __name__ == '__main__':
 		#sys.exit()
 
 
-
-
 	# summary statistics
 
 	testCase_new.get_summary_stats()
 
+	####################################
+	with open(report_output_dir +'/report','a') as report_file:
+		report_file.write('Summary statistics:\n')
+		report_file.write('Score   Mean   Median   SD   Min   Max\n')
+		report_file.write('score1   '+str(testCase_new.eda.summaryStat_score1.mu)+'   '+\
+			str(testCase_new.eda.summaryStat_score1.med)+'   '+\
+			str(testCase_new.eda.summaryStat_score1.sd)+'   '+\
+			str(testCase_new.eda.summaryStat_score1.min_val)+'   '+\
+			str(testCase_new.eda.summaryStat_score1.max_val)+'   \n')
+		report_file.write('score2   '+str(testCase_new.eda.summaryStat_score2.mu)+'   '+\
+			str(testCase_new.eda.summaryStat_score2.med)+'   '+\
+			str(testCase_new.eda.summaryStat_score2.sd)+'   '+\
+			str(testCase_new.eda.summaryStat_score2.min_val)+'   '+\
+			str(testCase_new.eda.summaryStat_score2.max_val)+'   \n')
+		report_file.write('difference   '+str(testCase_new.eda.summaryStat_score_diff_par.mu)+'   '+\
+			str(testCase_new.eda.summaryStat_score_diff_par.med)+'   '+\
+			str(testCase_new.eda.summaryStat_score_diff_par.sd)+'   '+\
+			str(testCase_new.eda.summaryStat_score_diff_par.min_val)+'   '+\
+			str(testCase_new.eda.summaryStat_score_diff_par.max_val)+'   \n\n')
+	####################################
+
+
 	# skewness test
-	testCase_new.eda.testParam = logic.dataAnalysis.skew_test(testCase_new.score_diff_par)[1]
+	skewness_test_res = logic.dataAnalysis.skew_test(testCase_new.score_diff_par)
+	gamma = skewness_test_res[0]
+	skewed = ''
+	if abs(gamma)>1:
+		skewed = 'highly skewed'
+	elif abs(gamma)<1 and abs(gamma)>0.5:
+		skewed = 'moderately skewed'
+	elif abs(gamma)<0.5:
+		skewed = 'roughly symmetric'
+
+	testCase_new.eda.testParam = skewness_test_res[1]
 
 
 	# normality test
@@ -111,6 +186,13 @@ if __name__ == '__main__':
 		score = testCase_new.score_diff_par, 
 		alpha = testCase_new.eda.normal_alpha)
 
+	####################################
+	with open(report_output_dir +'/report','a') as report_file:
+		report_file.write('Test statistic recommendation:\n')
+		report_file.write('-- normality: '+str(testCase_new.eda.normal)+'\n')
+		report_file.write('-- skewness: skewness measure is '+str(gamma)+'. Data distribution is '+skewed+'\n')
+		report_file.write('-- test statistic: '+str(testCase_new.eda.testParam)+'\n\n')
+	####################################
 
 	# recommend tests
 	list_of_tests = logic.dataAnalysis.recommend_test(testCase_new.eda.testParam,testCase_new.eda.normal)
@@ -126,6 +208,30 @@ if __name__ == '__main__':
 			not_preferred_test_list.append((i,j[1]))
 		if j[0] == -1:
 			inappro_test_list.append((i,j[1]))
+
+
+	####################################
+	with open(report_output_dir +'/report','a') as report_file:
+		report_file.write('Recommended tests:\n')
+		for i in recommended_test_list:
+			report_file.write('-- '+str(i[0])+'\n')
+		report_file.write('\n')
+
+		report_file.write('Recommended but not preferred tests:\n')
+		for i in not_preferred_test_list:
+			report_file.write('-- '+str(i[0])+'\n')
+		report_file.write('\n')
+
+		report_file.write('Inappropriate tests:\n')
+		if len(inappro_test_list) != 0:
+			for i in inappro_test_list:
+				report_file.write('-- '+str(i[0])+'\n')
+		else:
+			report_file.write('None\n')
+		report_file.write('\n\n')
+	####################################
+
+
 
 
 	print('Appropriate and recommended tests are:')
@@ -148,7 +254,8 @@ if __name__ == '__main__':
 
 	for i in inappro_test_list:
 		if i[0] == chosen_test:
-			chosen_test = input("Please choose from the list of recommended tests:\n")
+			chosen_test = input("Please choose from the list of appropriate tests:\n")
+
 
 	testCase_new.sigTest.testName = chosen_test
 
@@ -175,6 +282,16 @@ if __name__ == '__main__':
 	testCase_new.sigTest.pval = pval
 	testCase_new.sigTest.rejection = rejection
 
+	####################################
+	with open(report_output_dir +'/report','a') as report_file:
+		report_file.write('Testing results:\n')
+		report_file.write('-- significance test used: '+str(testCase_new.sigTest.testName)+'\n')
+		report_file.write('-- confidence interval ('+str(testCase_new.eda.testParam)+'): '+str(CI)+'\n')
+		report_file.write('-- p-value: '+str(testCase_new.sigTest.pval)+'\n')
+		report_file.write('-- reject H0: '+str(testCase_new.sigTest.rejection)+'\n')
+		report_file.write('-- H1: '+str(h1)+'\n\n')
+	####################################
+
 	### effect size calculation
 
 	print("------ Effect Size ------")
@@ -182,6 +299,15 @@ if __name__ == '__main__':
 	eff_size_est, es_CI = logic.effectSize.calc_eff_size(testCase_new.es.estimator, testCase_new.score_diff_par, es_alpha, es_B)
 
 	testCase_new.es.estimate = eff_size_est
+
+	####################################
+	with open(report_output_dir +'/report','a') as report_file:
+		report_file.write('Effect size:\n')
+		report_file.write('-- index: '+str(testCase_new.es.estimator)+'\n')
+		report_file.write('-- estimate: '+str(testCase_new.es.estimate)+'\n')
+		report_file.write('-- confidence interval: '+str(es_CI)+'\n\n')
+	####################################
+
 
 	### post power analysis
 
@@ -205,8 +331,15 @@ if __name__ == '__main__':
     
 	testCase_new.power.powerCurve = power_sampsize
 
+	####################################
+	with open(report_output_dir +'/report','a') as report_file:
+		report_file.write('Post-testing power analysis:\n')
+		report_file.write('obtained power: ' + str(list(testCase_new.power.powerCurve.values())[-1])+'\n')
 
-	print('------ Report ------')
+	####################################
+
+
+	print('------ Testing Results ------')
 
 	print('Test name: '+testCase_new.sigTest.testName)
 	print('Confidence interval: '+str(CI))
