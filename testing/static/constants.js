ff807d420e47d3ef39247f14826ea52c96b1e785
DeniseMak <dpm3@uw.edu>
2020-09-18 11:40:38 -0700

    dynamically set min_eu_size, fix error in regex, client-size validation warning

--- testing/static/constants.js
+++ testing/static/constants.js
@@ -1,9 +1,10 @@
 const fileLabelId = "fileLabel"
-const MAX_FILE_SIZE_IN_KB = 100
+const euSizeId = 'eval_unit_size'
+const MAX_FILE_SIZE_IN_KB = 10000  // 10000 for 10MB
 const MAX_NUM_SAMPLES = 100000
 const MAX_FILE_SIZE = '10 MB'
 // 'invalid' to disallow files over MAX_FILE_SIZE_IN_KB, 'warn' to just warn
-const LARGE_FILE_ACTION =  'warn' // or 'invalid'
+const LARGE_FILE_ACTION =  'invalid' // or 'warn'
 const FILE_TOO_LARGE_MESSAGE =
     "Error: The selected file must not be larger than " + MAX_FILE_SIZE
 const WARN_MESSAGE =
