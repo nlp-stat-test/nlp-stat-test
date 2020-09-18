29d2e295ed4b0301ef17df51a0d5e93322b34799
NLP-stat-test admin <denise.mak@outlook.com>
2020-09-18 02:49:48 -0700

    begin client-side line counting, add InputError handing

--- testing/static/constants.js
+++ testing/static/constants.js
@@ -1,9 +1,12 @@
 const fileLabelId = "fileLabel"
 const MAX_FILE_SIZE_IN_KB = 100
-const MAX_NUM_SAMPLES = 10000
+const MAX_NUM_SAMPLES = 100000
+const MAX_FILE_SIZE = '10 MB'
 // 'invalid' to disallow files over MAX_FILE_SIZE_IN_KB, 'warn' to just warn
 const LARGE_FILE_ACTION =  'warn' // or 'invalid'
 const FILE_TOO_LARGE_MESSAGE =
-    "Error: The selected file must not be larger than 100 kB"
+    "Error: The selected file must not be larger than " + MAX_FILE_SIZE
 const WARN_MESSAGE =
-    '<br> &nbsp;<span style="color:tomato;"><i>Warning: your file is over the size limit. The calculation will sample from a subset of the data.</i></span>'
+    '<br> &nbsp;<span style="color:tomato;"><i>Warning: your file is over the '+ MAX_FILE_SIZE
+    ' size limit. ' +
+    '<br> See the manual for instructions on running NLPStats locally.</i></span>'
